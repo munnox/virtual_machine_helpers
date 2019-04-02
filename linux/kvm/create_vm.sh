5fa13377345aac3d620fefbb2a9d2d19ba5f9f07
Robert Munnoch <munnox@hotmail.com>
2019-04-02 18:40:50 +0100

    Added an update and refactored the create vm

--- linux/kvm/create_vm.sh
+++ linux/kvm/create_vm.sh
@@ -1,8 +1,8 @@
 # Source <https://linuxconfig.org/how-to-create-and-manage-kvm-virtual-machines-from-cli>
 
 virt-install --name=ubuntu18.04-Image \
-    --vcpus=2 \
-    --memory=2048 \
-    --cdrom="/var/ubuntu-18.04.1-desktop-amd64.iso" \
-    --disk size=15 \
-    --os-variant=ubuntu18.04
+  --vcpus=2 \
+  --memory=2048 \
+  --cdrom="/var/ubuntu-18.04.1-desktop-amd64.iso" \
+  --disk size=15 \
+  --os-variant=ubuntu18.04
---
+++ linux/kvm/list_variants.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install libosinfo-bin
+
+# List Os variants
+osinfo-query os
