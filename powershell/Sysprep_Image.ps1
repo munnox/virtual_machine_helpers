f7233d713ad8c54828f5940f29c56829ccb48066
Robert Munnoch <munnox@hotmail.com>
2019-04-14 16:08:08 +0100

    Added a script to prep a windows VM first

---
+++ powershell/Sysprep_Image.ps1
@@ -0,0 +1,14 @@
+﻿# Sysprep an image
+
+# VM Image name
+$name = "vws";
+
+# Basic Creds
+$LocalUser = "$name\Administrator"
+$LocalPassword = ConvertTo-SecureString -String "passw0rd!" -AsPlainText -Force
+$LocalCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $LocalUser, $LocalPassword
+
+# Invoke the sysprep command on the remote VM using the credentials above
+Invoke-Command -VMName $name -Credential $LocalCredential -ScriptBlock {
+    C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown
+} -Verbose
